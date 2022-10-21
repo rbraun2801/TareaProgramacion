9157a419c9e21032654eeb9e1eafbc8806eda759
rbraun2801 <89944018+rbraun2801@users.noreply.github.com>
2022-10-21 16:11:17 -0300

    ALGORITMOS EJ1

---
+++ Ejercicio1Algoritmos.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int int main()
+{
+	/* codigo */
+	int nota; /*Creo la variable*/
+	scanf("%d",&nota); /*Solicito la variable*/
+	
+	if(nota>=6){ /*Compruebo si la nota es mayor o igual a 6 */
+		printf("APROBADO");
+	}else{
+		printf("DESAPROBADO");
+	}
+	return 0;
+}
