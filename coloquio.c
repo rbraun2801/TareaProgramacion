05a56b63de6e4dc63d331072103c080a922600f6
rbraun2801 <robertobraun87@gmail.com>
2022-11-15 01:25:04 -0300

    comentarios agregados

--- coloquio.c
+++ coloquio.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
+/*Creo todas laas funciones necesarias*/
+/*Funcion que imprime todos los asientos */
 void imprimirOcupacion(char filas[4][5]){
     int i, j;
     printf("  A B C D E\n");
-    for(i=0;i<4;i++){
+    for(i=0;i<4;i++){ /*Recorro el primer indice*/
         printf("%d ",i+1);
-        for(j=0; j<5;j++){
-        printf("%c ", filas[i][j]);
+        for(j=0; j<5;j++){ /*Recorro el segundo*/
+        printf("%c ", filas[i][j]); /*Imprimo el contenido de Filas en las posiciones de i y j*/
         }
         printf("\n");
     }
 }
+/*Funcion que utilizo para pasar la letra de scanf a numero y poder utilizarla en el indice*/
 int letraACol(char letra){
     int col;
     switch (letra)
@@ -32,6 +35,7 @@ int letraACol(char letra){
     }
     return col;
 }
+/*Funcion de dedicada a contar las ventas o los lugares ocupados*/
 int contarVentas(char filas[4][5]){
      int i, j;
      int ventas=0;
@@ -44,7 +48,8 @@ int contarVentas(char filas[4][5]){
 }
 return ventas;
 }
-void venta(char filas[4][5]){
+/*Funcion en la que genero una venta de un lugar o la cancelo*/
+void venta(char filas[4][5], char ocupacion){
     int n_entradas;
     printf("Cuantas entradas desea comprar?");
     scanf("%d",&n_entradas);
@@ -55,9 +60,10 @@ void venta(char filas[4][5]){
         printf("Por favor ingrese letra y fila");
         scanf(" %c %d",&letra,&fila);
         col=letraACol(letra);
-        filas[fila-1][col]='O';
+        filas[fila-1][col]= ocupacion;
         }
 };
+/*Funcion que crea el menu y devuelve el valor del mismo para ser analizado*/
 int menu(){
     int opcionMenu;
     printf("Por Favor Seleccione una opcion del siguiente menu:\n");
@@ -71,32 +77,38 @@ int menu(){
     scanf("%d", &opcionMenu);
     return opcionMenu;
 }
+
 int main()
 {
 char filas[4][5]={{'L','L','L','L','L'},{'L','L','L','L','L'},{'L','L','L','L','L'},{'L','L','L','L','L'}};
-int ventas;
+int ventas,opcion;
+char ocupacion;
 printf("Bienvenido al Sistema de Ventas del CINE ITS\n");
-int opcion;
-do{
+do{ /*Utilizo el do while para que ingrese al menos una vez*/
     /* Creo el Menu */
     opcion=menu();
     switch (opcion)
     {
     case 1:
-        /*Tratar de implementar una funcion para todo esto!*/
-        venta(filas);
+        /*Utilizo la funcion antes mencionada venta y paso el parametro O de ocupado junto a la matriz*/
+        ocupacion='O';
+        venta(filas,ocupacion);
         break;
     case 2:
-        venta(filas);
+        /*Utilizo la funcion antes mencionada venta y paso el parametro L de Libre junto a la matriz*/
+        ocupacion='L';
+        venta(filas,ocupacion);
         break;
     case 3:
+        /*Utilizo la funcion para mostrar todas las butacas*/
         imprimirOcupacion(filas);
         break;
     case 4:
+        /*Utilizo la funcion para contar la cantidad de asientos vendidos luego los multiplico por el costo de la entrada*/
         ventas=contarVentas(filas)*300;
         printf("La cantidad recaudada hasta este momento es $ %d",ventas);
         break;
     }
-}while(opcion!=5);
+}while(opcion!=5);/*La unica manera de salir es utilizando el 5*/
 return 0; 
 }
