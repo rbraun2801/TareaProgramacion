316381447fbe2f96a9e0c3289a63cc3f89d712b1
rbraun2801 <robertobraun87@gmail.com>
2022-11-23 20:14:39 -0300

    coloquio finalisimoo

--- coloquio.c
+++ coloquio.c
@@ -13,7 +13,7 @@ int venta(char filas[4][5], char ocupacion);
 void cancela(char filas[4][5], char ocupacion);
 /*Funcion que crea el menu y devuelve el valor del mismo para ser analizado*/
 int menu();
-
+/*COMIENZA EL MAAIN*/
 int main()
 {
 char filas[4][5]={{'L','L','L','L','L'},{'L','L','L','L','L'},{'L','L','L','L','L'},{'L','L','L','L','L'}};
@@ -23,30 +23,29 @@ printf("Bienvenido al Sistema para cines del ITS\n");
 do{ /*Utilizo el do while para que ingrese al menos una vez*/
     /* Creo el Menu */
     opcion=menu();
-    switch (opcion)
-    {
-    case 1:
-        /*Utilizo la funcion antes mencionada venta y paso el parametro O de ocupado junto a la matriz*/
-        ocupacion='O';
-        monto_a_pagar=monto_entrada*venta(filas,ocupacion);
-        printf("El Monto a pagar es: $%d\n",monto_a_pagar);
-        printf("La Compra fue realizada con exito\n");
-        break;
-    case 2:
-        /*Utilizo la funcion antes mencionada venta y paso el parametro L de Libre junto a la matriz*/
-        ocupacion='L';
-        cancela(filas,ocupacion);
-        break;
-    case 3:
-        /*Utilizo la funcion para mostrar todas las butacas*/
-        imprimirOcupacion(filas);
-        break;
-    case 4:
-        /*Utilizo la funcion para contar la cantidad de asientos vendidos luego los multiplico por el costo de la entrada*/
-        ventas=contarVentas(filas)*monto_entrada;
-        printf("La cantidad de entradas vendidas hasta el momento es: %d\n", contarVentas(filas));
-        printf("La cantidad recaudada hasta este momento es $ %d\n",ventas);
-        break;
+    switch (opcion){
+        case 1:
+            /*Utilizo la funcion antes mencionada venta y paso el parametro O de ocupado junto a la matriz*/
+            ocupacion='O';
+            monto_a_pagar=monto_entrada*venta(filas,ocupacion);
+            printf("El Monto a pagar es: $%d\n",monto_a_pagar);
+            printf("La Compra fue realizada con exito\n");
+            break;
+        case 2:
+            /*Utilizo la funcion antes mencionada venta y paso el parametro L de Libre junto a la matriz*/
+            ocupacion='L';
+            cancela(filas,ocupacion);
+            break;
+        case 3:
+            /*Utilizo la funcion para mostrar todas las butacas*/
+            imprimirOcupacion(filas);
+            break;
+        case 4:
+            /*Utilizo la funcion para contar la cantidad de asientos vendidos luego los multiplico por el costo de la entrada*/
+            ventas=contarVentas(filas)*monto_entrada;
+            printf("La cantidad de entradas vendidas hasta el momento es: %d\n", contarVentas(filas));
+            printf("La cantidad recaudada hasta este momento es $ %d\n",ventas);
+            break;
     }
 }while(opcion!=5);/*La unica manera de salir es utilizando el 5*/
 return 0; 
@@ -57,30 +56,29 @@ void imprimirOcupacion(char filas[4][5]){
     for(i=0;i<4;i++){ /*Recorro el primer indice*/
         printf("%d ",i+1);
         for(j=0; j<5;j++){ /*Recorro el segundo*/
-        printf("%c ", filas[i][j]); /*Imprimo el contenido de Filas en las posiciones de i y j*/
+            printf("%c ", filas[i][j]); /*Imprimo el contenido de Filas en las posiciones de i y j*/
         }
         printf("\n");
     }
 }
 int letraACol(char letra){
     int col;
-    switch (letra)
-    {
-    case 'A':
-        col=0;
-        break;
-    case 'B':
-        col=1;
-        break;
-    case 'C':
-        col=2;
-        break;
-    case 'D':
-        col=3;
-        break;
-    case 'E':
-        col=4;
-        break;
+    switch (letra){
+        case 'A':
+            col=0;
+            break;
+        case 'B':
+            col=1;
+            break;
+        case 'C':
+            col=2;
+            break;
+        case 'D':
+            col=3;
+            break;
+        case 'E':
+            col=4;
+            break;
     }
     return col;
 }
@@ -113,29 +111,29 @@ int venta(char filas[4][5], char ocupacion){
         }else{
         filas[fila-1][col]= ocupacion;
         }
-        }
+    }
 }
 void cancela(char filas[4][5], char ocupacion){
     int n_entradas,fila, col;
     char letra;
     if (contarVentas(filas)>0){
-    printf("Cuantas entradas desea cancelar?");
-    scanf("%d", &n_entradas);
-    for(int i=0;i<n_entradas;i++){
-        imprimirOcupacion(filas);
-        printf("Por favor ingrese letra y fila: ");
-        scanf(" %c %d",&letra,&fila);
-        col=letraACol(letra);
-        if (filas[fila-1][col]=='L'){
+        printf("Cuantas entradas desea cancelar?");
+        scanf("%d", &n_entradas);
+        for(int i=0;i<n_entradas;i++){
+            imprimirOcupacion(filas);
+            printf("Por favor ingrese letra y fila: ");
+            scanf(" %c %d",&letra,&fila);
+            col=letraACol(letra);
+            if (filas[fila-1][col]=='L'){
             printf("El asiento se encuentra Libre. NO SE PUEDE CANCELAR\n");
             cancela(filas,ocupacion);
-        }else{
-        filas[fila-1][col]= ocupacion;
+            }else{
+            filas[fila-1][col]= ocupacion;
+            }
         }
-        }
-        }else{
-            printf("NO EXISTEN LOCALIDADES VENDIDAS\n");
-        }
+    }else{
+        printf("NO EXISTEN LOCALIDADES VENDIDAS\n");
+    }
 }
 int menu(){
     int opcionMenu;
